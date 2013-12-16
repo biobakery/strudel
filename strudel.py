c80827db257dbf98bca1801be616512f71aeb731
Joseph Moon <moon.yosup@gmail.com>
2013-12-15 19:09:33 -0500

    Check helper functions added.

--- strudel.py
+++ strudel.py
@@ -14,11 +14,17 @@ URL
 Notes 
 -----------
 
+* Invariance principle: generally, everything should be a list or array when passed into distribution generation functions, if they are not, 
+match up so length is preserved 
+
+	** Parameters passed to distributions or functions should be _tuples_ 
+	** The object enclosing multiple parameters should be _lists_ 
+
 * Let's not reinvent the wheel; for more complicated Bayesian networks, should look at PyMC and implementations such as bnlearn 
 
 * The base distribution is _always_ the prior and the base parameters are _always_ the hyperparameters 
 * Known useful generations 
-	* zero- and one-inflated beta distributions
+	* zero- and one-inflated beta distributions 
 	* zero- and one-inflated gaussian distributions 
 	* dirichlet-type data (true and false zeros)
 	* non-parametric generation of time series data 
@@ -30,7 +36,7 @@ Notes
 	* Clusterd Data 
 	* Non-parametric and parametric estimates for mutual information calculation 
 * Build class in such a way that making distributions arbitrarily complicated is easy to write down 
-* Make meta-strudel a reality 
+* Make meta-strudel a reality: I think at that point using PyMC, PyMix, and python modules for creating networks is necessary 
 
 
 """
@@ -43,8 +49,8 @@ import sys
 import itertools 
 
 ####### namespace for distributions 
-from scipy.stats import invgamma, norm, uniform, logistic
-from numpy.random import normal, multinomial
+from scipy.stats import invgamma, norm, uniform, logistic, gamma
+from numpy.random import normal, multinomial, dirichlet 
 
 ####### namespace for plotting 
 from pylab import hist, plot, figure
@@ -56,11 +62,19 @@ class Strudel:
 
 	def __init__( self ):
 
+		### Wrappers to make frozen rv objects for those distributions that are from numpy 
+
 		class linear: ## wrapper for the linear "distribution" method 
 			def __init__( self, param1 = None, param2 = None ):
 				self.object = None 
 				if param1 and param2:
 					self.object = lambda shape: numpy.linspace( param1, param2, shape )
+
+		class dirichlet: ## wrapper for the dirichlet method 
+			def __init__( self, alpha = None ):
+				self.object = None 
+				if alpha: 
+					self.object = lambda shape: dirichlet( alpha, shape )
 
 			def rvs( self, shape ):
 				return self.object( shape )
@@ -68,7 +82,10 @@ class Strudel:
 		self.hash_distributions	= { "uniform"	: uniform, 
 									"normal"	: norm, 
 									"linear"	: linear,
+									"gamma"		: gamma, 
 									"invgamma"	: invgamma,
+									"lognormal"	: lognorm,
+									"dirichlet" : dirichlet 
 									}
 
 		self.hash_conjugate		= { "normal" : ("normal", "invgamma"),
@@ -164,6 +181,81 @@ class Strudel:
 	def _eval_rvs( self, aBase, aParam, pEval = () ):
 		return self._rvs( self._eval( aBase, aParam, "*")  , pEval)
 
+	def _convert( self, pObj, iLen = None ):
+		"""
+		Convert to iterable, if applicable 
+		"""
+
+		if not iLen:
+			iLen = 1 
+		return [pObj for _ in range(iLen)]
+
+	def _check( self, pObject, pType ):
+		"""
+		Wrapper for type checking 
+		"""
+
+		if not (isinstance(pObject,list) or isinstance(pObject,tuple) or isinstance(pObject,array)):
+			aType = [pType]
+		else:
+			aType = pType 
+
+		return reduce( lambda x,y: x or y, [isinstance( pObject, t ) for t in aType], False )
+
+	def _is_list( self, pObject ):
+		return self._check( pObject, list )
+
+	def _is_tuple( self, pObject ):
+		return self._check( pObject, tuple )
+
+	def _is_iter( self, pObject ):
+		"""
+		Is the object a list or tuple? 
+		Disqualify string as a true "iterable" in this sense 
+		"""
+
+		return self._check( pObject, [list, tuple, array] )
+
+
+	def _make_invariant( self, pObject, pMatch ):
+		"""
+		Invariance principle; match up pObject with an invariance imposed by pMatch 
+
+		Most used case is as a wrapper for the homogeneous case 
+		
+		[0] -> [0,0,0,...] etc 
+		"""
+
+		aObject = None 
+
+		iMatch = None 
+
+		if self._check( pMatch, int ):
+			iMatch = pMatch 
+		elif self._is_iter( pMatch ):
+			iMatch = len( pMatch )
+
+		if iMatch:
+			if self._is_list( pObject ):
+				if len(  pObject ) == 1:
+					aObject = [pObject for _ in range(iMatch)]
+				else:
+					raise Exception("Length of object does not match specified match function")
+
+			elif self._is_tuple( pObject ):
+				aObject = [pObject for _ in range(iMatch)]
+
+			else:
+				aObject = [pObject for _ in range(iMatch)]
+
+		return aObject 
+
+
+	def _categorical( self, aProb, aCategory = None ):
+		if not aCategory:
+			aCategory = range(len(aProb))
+		return next( itertools.compress( aCategory, multinomial( 1, aProb )  ) )
+		
 	#========================================#
 	# Public functions 
 	#========================================#
@@ -199,7 +291,7 @@ class Strudel:
 		
 		return self._eval_rvs( H, self.base_param, ( shape if iRow else iCol ) )
 
-	def randmix( self, shape = 10, param = [(0,1), (1,1)], pi = [0.5,0.5] ):
+	def randmix( self, shape = 10, param = [(0,1), (5,1)], pi = [0.5,0.5], adj = False ):
 		"""
 		Draw N copies from a mixture distribution with pdf $ pi^T * H( \cdot | param ) $
 		
@@ -225,40 +317,28 @@ class Strudel:
 
 		H = self.base_distribution 
 		
+		K = len( pi )
+
+		### Is the base distribution homogeneous or heterogeneous? 
+		if isinstance( self.base, list ) or isinstance( self.base, tuple ):
+			H = H
+		elif isinstance( self.base, str ):
+			H = [H for _ in K]
+
 		assert( len( param ) == len( pi ) )
 		
-		aOut = [] 
-
-		K = len( param ) 
+		aOut = []  
 		
 		def _draw():
-			return H( *[x for x in itertools.compress( param, multinomial( 1, pi ) )][0] ).rvs()
-				
+			z = self._categorical( pi )
+			return self._eval_rvs( H[z], param[z], () )
+
 		return [[ _draw() for _ in range(iCol)] for _ in (range(iRow) if iRow else range(1)) ]
 
 
-	def randclust( self, param ):
-		"""
-		Draw clustered data; linked through bayesian net 
 
-		Parameters
-		-----------
-
-		Returns
-		----------
-
-		Notes
-		----------
-
-			Example
-			{w} = G < -- x --> F = {y,z}
-
-		"""
-		pass 
-
-
-	def generate_clustered_data( self, num_clusters = 3, num_children = 3, num_examples = 10, dist = ["norm", "norm", "norm"], 
-			param = [((0,0.01),(1,1)), ((5,0.01),(2,1)), ((10,0.01),(3,1))]):
+	def randclust( self, num_clusters = 3, num_children = 3, num_examples = 10, dist = ["normal", "normal", "normal"], 
+			param = [((0,0.01),(1,1)), ((5,0.01),(2,1)), ((10,0.01),(3,1))], adj = False ):
 		"""
 
 		Generate clustered data by graphical model representation.
@@ -281,14 +361,17 @@ class Strudel:
 		------------
 			Z : numpy ndarray 
 				p x n matrix, with clustered variables 
+		
+
+		Notes
+		----------
+
+			Example
+			{w} = G < -- x --> F = {y,z}
+
 		"""
 
-		#num_clusters = None 
 
-		#if isinstance( self.base, str ):
-		#	num_clusters = 1 
-		#elif isinstance( self.base, list ) or isinstance( self.base, tuple ):
-		#	num_clusters = len( self.base )
 
 		"""
 		### Some exception handling 
@@ -303,9 +386,9 @@ class Strudel:
 		if number_clusters != len(dist)
 		"""
 
-		aOut = [] 
+		aDist = [getattr(self, d) for d in dist]
 
-		
+		aOut = [] 
 
 		zip_param = zip(*param)
 
@@ -321,7 +404,6 @@ class Strudel:
 
 		for k in range(num_clusters):
 
-
 			#prior_mu, prior_sigma = atHyperMean[k]
 			#alpha, beta = atHyperVar[k]
 
@@ -330,7 +412,11 @@ class Strudel:
 			
 			## atHyper has columns of hyperparameters 
 
-			dist_param = [ self.base_distribution(*t).rvs() for t in atHyper ] 
+			#dist_param = self._eval_rvs( self.base_distribution, atHyper )
+
+			self._eval_rvs( self.base_distribution, atHyper )
+
+			#dist_param = [ self.base_distribution(*t).rvs() for t in atHyper ] 
 
 			for j in range(num_children):
 
@@ -347,6 +433,18 @@ class Strudel:
 	#=============================================================#
 
 	def adjacency( self ):
+		"""
+		Produce probabilistic adjacency matrix describing the linkage pattern in 
+		the synthetic data produced by strudel.
+
+		Parameters
+		-------------
+
+		Returns 
+		----------
+
+
+		"""
 		pass 
 
 	#==============================================================================#
@@ -354,7 +452,6 @@ class Strudel:
 	## Good reference is http://cran.r-project.org/web/packages/mlbench/index.html 
 	## Incorporate useful elements from it in the future 
 	#==============================================================================#
-
 
 	def identity( self, shape = 100 ):
 		H = self.base_distribution( *self.base_param )
