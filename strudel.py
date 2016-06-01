64a67cf507e3b5a7b660468f0d7f69d8586d35ab
gholamali_rahnavard <gholamali_rahnavard@localhost>
2016-06-01 13:36:14 -0400

    synthetic data with imbalanced clusters and orthogonal base vectors

--- strudel.py
+++ strudel.py
@@ -2077,7 +2077,7 @@ class Strudel:
 		#print (numpy.dot(U,S)).shape
 		#print np.allclose(w.T, np.dot(U, np.dot(S, V)))
 		return numpy.dot(U,S).T
-	def imbalanced_synthetic_dataset_uniform(self, D, N, B, within_noise = 0.5, between_noise = 0.5, association_type = 'parabola' ):
+	def imbalanced_synthetic_dataset_uniform(self, D, N, B, cluster_percentage = 1, within_noise = 0.5, between_noise = 0.5, association_type = 'parabola' ):
 		"""
 			D: int
 				number of features
@@ -2090,52 +2090,97 @@ class Strudel:
 		"""
 		
 		X = numpy.random.uniform(low=-1,high=1 ,size=(D,N))
+		Y = numpy.random.uniform(low=-1,high=1,size=(D,N))
+		
 		common_base = numpy.random.uniform(low=-1,high=1 ,size=(B+1,N))
-		common_base = self.orthogonalize_matrix(common_base)
 		X_base = numpy.random.uniform(low=-1,high=1 ,size=(D,N))
 		Y_base = numpy.random.uniform(low=-1,high=1 ,size=(D,N))
-		Y = numpy.random.uniform(low=-1,high=1,size=(D,N))
+		
+		X_base = self.orthogonalize_matrix(X_base)
+		Y_base = self.orthogonalize_matrix(Y_base)
+		#X = self.orthogonalize_matrix(X)
+		#Y = self.orthogonalize_matrix(Y)
+		#common_base = self.orthogonalize_matrix(common_base)
 		A = numpy.zeros( (D,D) )
-		blockSize = int(round(D/B+.5))
+		blockSize = int(round(D/B))
 		#print D, B, blockSize
 		'''for i in range(0,D,blockSize):
 			for j in range(i,i+blockSize):
 				if j < D:
 					X[j]= [X[i,k]  + numpy.random.normal(0,.1,1) for k in range(len(X[j]))]
 		'''
-		number_associated_blocks = max([int(B/3) , 1])
-		assoc1 = [[] for i in range(number_associated_blocks)]
-		assoc2 = [[] for i in range(number_associated_blocks)]
+		if association_type == "L":
+			common_base = numpy.hstack((numpy.random.uniform(low=-1.0,high=-1.0 ,size=(B+1,N/2)), numpy.random.uniform(low=-1,high=50 ,size=(B+1,N/2))))
+			for l in range(B+1):
+				common_base[l]= numpy.random.permutation(common_base[l])
+		else:
+			common_base = numpy.random.uniform(low=-1,high=1 ,size=(B+1,N))
+		if association_type == "L":
+			common_base_Y = numpy.random.uniform(low=-1,high=1 ,size=(B+1,N))
+			for l in range(B+1):
+				common_base_Y[l] = [numpy.random.uniform(low=l,high=100, size=1)  if common_base[l,k] < -0.999 else numpy.random.uniform(low=l,high=l, size=1) for k in range(N)]
+			common_base_Y = self.orthogonalize_matrix(common_base_Y)
+		#common_base = self.orthogonalize_matrix(common_base)
+		
+		number_associated_blocks =  max([int(B * cluster_percentage) , 1])
+		assoc1 = [[] for i1 in range(number_associated_blocks)]
+		assoc2 = [[] for j1 in range(number_associated_blocks)]
 		
 		from random import randrange
-		for i in range(D):
+		'''for i in range(int(D*cluster_percentage)):
 			r = randrange(0,B)
 			X[i]= [X_base[r,k]  + within_noise * numpy.random.uniform(low=-1,high=1 ,size=1) for k in range(N)]
 			
-		for i in range(D):
+		for i in range(int(D*cluster_percentage)):
 			r = randrange(0,B)
 			Y[i]= [Y_base[r,k]  + within_noise * numpy.random.uniform(low=-1,high=1 ,size=1) for k in range(N)]
-			
-		r = numpy.random.randint(D, size=int(blockSize* number_associated_blocks))
-		for i in r:
-			l= randrange(0,int(number_associated_blocks))
-			X[i]= [common_base[l][k] +  within_noise * numpy.random.uniform(low=-1,high=1 ,size=1) for k in range(N)]
+		'''	
+		#r = numpy.random.randint(D, size=int(D*cluster_percentage))
+		for i in range(int(D*cluster_percentage)):
+			l= randrange(0,number_associated_blocks)
+			numpy.random.seed()
+			if association_type == "L":
+						X[i]= [common_base[l,k]+ within_noise * numpy.random.uniform(low=-.1,high=.1, size=1) for k in range(N)]
+			elif association_type == "log":
+				X[i]= [math.fabs(common_base[l,k])  + within_noise * numpy.random.uniform(low=0,high=1 ,size=1) for k in range(N)]
+			else:	
+				X[i]= [common_base[l,k]  + within_noise * numpy.random.uniform(low=-1,high=1 ,size=1) for k in range(N)]
+			#X[i]= [common_base[l][k] +  within_noise * numpy.random.uniform(low=-1,high=1 ,size=1) for k in range(N)]
 			assoc1[l].append(i)
 		
 		noise_num = numpy.random.randint(N, size=int(N*between_noise))
-		for i in r:
-			l= randrange(0,int(number_associated_blocks))
+		for i in  range(int(D*cluster_percentage)):
+			l= randrange(0,number_associated_blocks)
 			#slope = 1.0 +numpy.random.random_sample()#slope = numpy.random.random_sample()
+			numpy.random.seed()
 			if association_type == "parabola":
-				Y[i]= [common_base[l][k] * common_base[l][k]  + within_noise * math.sqrt(math.fabs(numpy.random.uniform(low=-1,high=1 ,size=1)))  for k in range(N)]
-				for index,b in enumerate(noise_num):
-					Y[i][b] = Y[i][index]
-				assoc2[l].append(i)
-			elif association_type == "linear":
-				Y[i]= [common_base[l][k] + within_noise * numpy.random.uniform(low=-1,high=1 ,size=1)  for k in range(N)]
-				for index,b in enumerate(noise_num):
-					Y[i][b] = Y[i][index]
-				assoc2[l].append(i)
+				Y[i]= [common_base[l,k]*common_base[l,k]  + within_noise *math.sqrt(math.fabs(numpy.random.uniform(low=-1,high=1 ,size=1))) for k in range(N)]
+			elif association_type == "line":
+				Y[i]= [common_base[l,k]  + within_noise * numpy.random.uniform(low=-1,high=1 ,size=1) for k in range(N)]
+			elif association_type == "sine":
+				Y[i]= [.5* math.sin(math.pi * common_base[l,k]*1.5)  + within_noise * numpy.random.uniform(low=-1,high=1 ,size=1)  for k in range(N)]
+			elif association_type == "log":
+				Y[i]= [math.log(math.fabs(common_base[l,k]))  + within_noise *math.fabs(numpy.random.uniform(low=0,high=1 ,size=1)) for k in range(N)]
+			elif association_type == "step":
+				p1 = numpy.percentile(common_base[l], 25)
+				p2 = numpy.percentile(common_base[l], 50)
+				p3 = numpy.percentile(common_base[l], 75)
+				#NOISE = within_noise *numpy.random.uniform(low=-1,high=1 ,size=1)
+				#Y[j]= [ 0 if common_base[l,k] <p1 else  1 + within_noise *numpy.random.uniform(low=-1,high=1 ,size=1)\
+				#	 if common_base[l,k] <p2 else 2+within_noise *numpy.random.uniform(low=-1,high=1 ,size=1)\
+				#	 if common_base[l,k] <p3 else 3 + within_noise *numpy.random.uniform(low=-1,high=1 ,size=1)  for k in range(N)]
+				Y[i]= [ 2.0 +within_noise *numpy.random.uniform(low=-1,high=1 ,size=1) if common_base[l,k] < p1 else  1.0 + within_noise *numpy.random.uniform(low=-1,high=1 ,size=1)\
+					if common_base[l,k] < p2 else  3.0 + within_noise *numpy.random.uniform(low=-1,high=1 ,size=1) if common_base[l,k] < p3  else
+					0.0 + within_noise *numpy.random.uniform(low=-1,high=1 ,size=1) for k in range(N)]
+			elif association_type == "L":
+				Y[i] = [ common_base_Y[l,k] + within_noise * numpy.random.uniform(low=-1,high=1, size=1) for k in range(N)]
+				#Y[j]= [ numpy.random.uniform(low=10,high=100, size=1) * common_base[l,k] if common_base[l,k] < -0.8 else numpy.random.uniform(low=.2,high=.5, size=1) for k in range(N)]
+			elif association_type =="happyface":
+				Y[i] = [ self.happyface(common_base[l,k]) + within_noise * numpy.random.uniform(low=-.1,high=.1, size=1) for k in range(N)]
+				
+			for index,b in enumerate(noise_num):
+				Y[i][b] = Y[i][index]
+			assoc2[l].append(i)
 
 		for a in range(number_associated_blocks):
 			#print assoc1[a], assoc2[a]
@@ -2168,6 +2213,7 @@ class Strudel:
 		else:
 			common_base = numpy.random.uniform(low=-1,high=1 ,size=(B+1,N))
 		common_base = self.orthogonalize_matrix(common_base)
+		
 		assoc = [[] for i in range((B+1))]
 		l = 0
 		for i in range(0,int(D*cluster_percentage),blockSize):
@@ -2214,7 +2260,7 @@ class Strudel:
 							if common_base[l,k] < p2 else  3.0 + within_noise *numpy.random.uniform(low=-1,high=1 ,size=1) if common_base[l,k] < p3  else
 							0.0 + within_noise *numpy.random.uniform(low=-1,high=1 ,size=1) for k in range(N)]
 					elif association_type == "L":
-						Y[j] = [ common_base_Y[l,k] + within_noise * numpy.random.uniform(low=-.1,high=.1, size=1) for k in range(N)]
+						Y[j] = [ common_base_Y[l,k] + within_noise * numpy.random.uniform(low=-1,high=1, size=1) for k in range(N)]
 						#Y[j]= [ numpy.random.uniform(low=10,high=100, size=1) * common_base[l,k] if common_base[l,k] < -0.8 else numpy.random.uniform(low=.2,high=.5, size=1) for k in range(N)]
 					elif association_type =="happyface":
 						Y[j] = [ self.happyface(common_base[l,k]) + within_noise * numpy.random.uniform(low=-.1,high=.1, size=1) for k in range(N)]
